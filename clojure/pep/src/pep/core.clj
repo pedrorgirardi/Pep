4adc55318d7656a411c925b36b345d97b9b3eb8d
Pedro Girardi <pedro@plando.work>
2021-05-29 17:25:05 +0700

    Run analysis

--- clojure/pep/src/pep/core.clj
+++ clojure/pep/src/pep/core.clj
@@ -1,2 +1,13 @@
 (ns pep.core
   (:require [clj-kondo.core :as clj-kondo]))
+
+(comment
+  
+  (def analysis
+    (clj-kondo/run! {:lint ["/Users/pedro/Developer/velos/rex.web/src/rex/web/app.cljs"]
+                     :cache-dir "/Users/pedro/Developer/velos/rex.web"
+                     :config {:output {:analysis true}}}))
+  
+  (keys (:analysis analysis))
+  
+  )
