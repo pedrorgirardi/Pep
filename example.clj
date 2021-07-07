360e91d34918b1fd10315755a814b62c81a8023f
Pedro Girardi <pedro@plando.work>
2021-07-07 11:59:27 +0700

    Examples

--- example.clj
+++ example.clj
@@ -1,25 +1,29 @@
+(let [{:person/keys [age]} {:person/age 33}]
+  
+  :person/age
+  
+  age)
+
+
 (let [x 1]
-  x
-  x
   x)
 
+
 (def a 1)(def b 2)
+
 
 (defn f "Docstring." [x] x)
 
 
 (f 1)
 
-(let [a 1] nil)
-
-(let [a 1]
-  a
-  a)
 
 (let [f (fn f [x] x)]
   (f)
   (f))
 
+
 (inc "")
+
 
 (map inc 1)
