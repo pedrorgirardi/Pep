457d29652fbf1344c1e9e1bac6a5bde1193b4e4a
Pedro Girardi <prg@fastmail.com>
2022-05-19 07:03:10 -0300

    Comments

--- pep.py
+++ pep.py
@@ -810,6 +810,9 @@ def goto(window, location, flags=sublime.ENCODED_POSITION):
                 view = window.open_file(f"{filename}:{line}:{column}", flags=flags)
                 view.set_scratch(False)
                 view.set_read_only(True)
+
+                # Filename doesn't match the namespace name because it's a temp file,
+                # and that's why the linter :namespace-name-mismatch is disabled.
                 view.settings().set(S_PEP_CLJ_KONDO_CONFIG, "{:linters {:namespace-name-mismatch {:level :off} :unused-binding {:level :off}} :output {:analysis {:arglists true :locals true :keywords true :java-class-usages true} :format :json :canonical-paths true} }")
 
             open_jar(filename, open_file)
