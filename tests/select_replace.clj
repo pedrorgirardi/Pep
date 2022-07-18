f0cd1e177b9e2d308a0573fc7ca55387238435e1
Pedro Girardi <prg@fastmail.com>
2022-07-18 06:45:02 -0300

    Clojure file with examples for testing select and replace

---
+++ tests/select_replace.clj
@@ -0,0 +1,37 @@
+{:person/firstname "Pedro"
+ :person/lastname "Girardi"}
+
+:person/firstname
+
+:person/lastname
+
+:person/firstname
+
+#:person {:firstname ""
+          :lastname ""}
+
+
+#:person {:firstname ""
+          :lastname ""}
+
+
+(fn [{:keys [person/firstname]}])
+
+:firstname
+
+:firstname
+
+(require '[clojure.string :as str])
+
+str/join
+
+str/join
+
+str/replace
+
+str/replace
+
+clojure.string/replace
+
+clojure.string/replace
+
