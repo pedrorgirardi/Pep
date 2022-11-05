75d2f547a57bb0f1326f66029f8f8582cf9453c9
Pedro Girardi <prg@fastmail.com>
2022-11-05 16:42:56 -0300

    Fix extra double quotes

--- pep.py
+++ pep.py
@@ -2550,7 +2550,7 @@ def annotate_view(view):
     def finding_minihtml(finding):
         return f"""
         <body>
-        <div">
+        <div>
             <span style="font-size:{annotation_font_size(view.window())}">{htmlify(finding["message"])}</span></div>
         </div>
         </body>
