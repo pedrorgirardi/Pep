080a3ad940ab9873b2281436213c19816916fd52
Pedro Girardi <prg@fastmail.com>
2023-10-31 12:25:39 -0300

    TODO Symbols

--- tests/symbols.clj
+++ tests/symbols.clj
@@ -1,3 +1,11 @@
+;; Problem:
+;; Pep finds usages of these symbols in paths,
+;; but from a var it doesn't find a symbol.
+;;
+;; Solution:
+;; Searching for usages of symbol or var should
+;; also include symbols.
+
 'clojure.core/map
 
 'example/foo
