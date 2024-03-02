0ba50b2de4ac8a575e8aaa86b9f5cf78b8d64eff
pedrorgirardi <585191+pedrorgirardi@users.noreply.github.com>
2024-03-02 07:31:26 -0300

    Server Client WIP

--- src/client.py
+++ src/client.py
@@ -1,20 +1,58 @@
-import socket
 import json
+import os
+import socket
+import tempfile
+import threading
 
-# Define the path to the Unix socket
-socket_path = "/tmp/pep.socket"
 
-# Data to be sent
-data = {"op": "hello"}
+# Initialize a threading lock
+lock = threading.Lock()
 
-# Create a socket object for Unix Domain Socket communication
-with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client_socket:
-    # Connect to the server
-    client_socket.connect(socket_path)
-    
-    # Send data encoded in JSON
-    client_socket.sendall(json.dumps(data).encode('utf-8'))
-    
-    # Optionally, receive a response from the server
+
+def default_path():
+    return os.path.join(tempfile.gettempdir(), "pep.socket")
+
+
+def send_message(client_socket, data):
+    # Send data until either all data has been sent or an error occurs.
+    #
+    # None is returned on success.
+    # On error, an exception is raised,
+    # and there is no way to determine how much data, if any, was successfully sent.
+    client_socket.sendall(json.dumps(data).encode("utf-8"))
+
+
+def receive_message(client_socket):
+    # Receive data from the socket.
+    # The return value is a bytes object representing the data received.
     response = client_socket.recv(1024)
-    print("Received:", response.decode('utf-8'))
+
+    return json.loads(response.decode("utf-8"))
+
+
+def req(client_socket, data):
+    with lock:
+        send_message(client_socket, data)
+
+        return receive_message(client_socket)
+
+
+def diagnostics(client_socket, root_path):
+    data = {
+        "op": "diagnostics",
+        "root-path": root_path,
+    }
+
+    return req(client_socket, data)
+
+
+if __name__ == "__main__":
+    # Create a socket object for Unix Domain Socket communication
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect(default_path())
+
+        data = {"op": "foo"}
+
+        response = req(client_socket, data)
+
+        print(response)
