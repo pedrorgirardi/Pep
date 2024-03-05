80b457aac67fb14f9079bbc0effbc27401d6f6a4
pedrorgirardi <585191+pedrorgirardi@users.noreply.github.com>
2024-03-05 17:14:27 -0300

    Server test

--- server/test/pep/handler_test.clj
+++ server/test/pep/handler_test.clj
@@ -4,43 +4,9 @@
    [clojure.test :refer [deftest is]]
 
    [pep.specs]
-   [pep.server :as server]
-   [pep.handler :as handler])
-
-  (:import
-   (java.nio.channels SocketChannel)
-   (java.net UnixDomainSocketAddress)))
+   [pep.handler :as handler]))
 
 (set! *warn-on-reflection* true)
-
-(defn request [^SocketChannel c r]
-  (server/write! c r)
-  (server/with-timeout #(server/read! c) 2))
-
-(deftest handle-default-test
-  (let [address (server/random-address)
-
-        stop (server/start {:address address})]
-
-    (is (= {:success "nop"}
-          (with-open [c (SocketChannel/open ^UnixDomainSocketAddress address)]
-            (request c {:op "Hello!"}))))
-
-    (stop)))
-
-(deftest handle-error-test
-  (let [address (server/random-address)
-
-        stop (server/start {:address address})]
-
-    (is (= {:error
-            {:message "Bad handler."
-             :data {:foo "bar"}}}
-          (with-open [c (SocketChannel/open ^UnixDomainSocketAddress address)]
-            (request c {:op "error"}))))
-
-    (stop)))
-
 
 (deftest handle-namespace-definitions-test
   (let [root-path (System/getProperty "user.dir")]
