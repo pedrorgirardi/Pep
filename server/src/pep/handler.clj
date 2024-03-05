47f9fd39d8d69cee2dc5a02f810351cbf1b063bc
pedrorgirardi <585191+pedrorgirardi@users.noreply.github.com>
2024-03-04 21:34:29 -0300

    Handle namespaces

--- server/src/pep/handler.clj
+++ server/src/pep/handler.clj
@@ -3,6 +3,8 @@
    [clojure.java.io :as io]
    [clojure.data.json :as json]
 
+   [next.jdbc :as jdbc]
+
    [pep.ana :as ana]))
 
 (defmulti handle
@@ -40,14 +42,44 @@
 
     {:success {:summary summary}}))
 
+(defmethod handle "namespace-definitions"
+  [{:keys [root-path]}]
+  (let [query "SELECT
+                  \"name\",
+                  \"row\",
+                  \"end-row\",
+                  \"col\",
+                  \"end-col\",
+                  \"filename\"
+                FROM
+                  '%s'
+                WHERE
+                  _sem = 'namespace-definitions'
+                ORDER BY
+                  \"name\""
+
+        query (format query (io/file root-path ".pep" "*.json"))
+
+        rows (with-open [db (jdbc/get-connection "jdbc:duckdb:")]
+               (jdbc/execute! db ["INSTALL json; LOAD json;"])
+               (jdbc/execute! db [query]))]
+
+    {:success rows}))
+
 (comment
+
+  (def root-path "/Users/pedro/Library/Application Support/Sublime Text/Packages/Pep/server")
 
   (handle
     {:op "diagnostics"
-     :root-path "/Users/pedro/Library/Application Support/Sublime Text/Packages/Pep/server"})
+     :root-path root-path})
 
   (handle
     {:op "analyze"
-     :root-path "/Users/pedro/Library/Application Support/Sublime Text/Packages/Pep/server"})
+     :root-path root-path})
+
+  (handle
+    {:op "namespace-definitions"
+     :root-path root-path})
 
   )
