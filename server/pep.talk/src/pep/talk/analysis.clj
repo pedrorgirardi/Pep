cb269a0176e714f316889dabcf2d3dd74df100c6
pedrorgirardi <585191+pedrorgirardi@users.noreply.github.com>
2024-03-25 20:29:19 -0300

    Analysis example

---
+++ server/pep.talk/src/pep/talk/analysis.clj
@@ -0,0 +1,11 @@
+(ns pep.talk.analysis
+  "Namespace to serve as an example of clj-kondo data anlysis."
+  (:require
+   [clojure.string :as str :refer [blank?]]))
+
+(defn hello []
+  (println "Hello, there!"))
+
+'hello
+
+`str/blank?
